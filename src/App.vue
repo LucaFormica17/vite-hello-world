f58a02161a93769c7f55531e60be12dc204c317c
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2024-01-10 17:07:08 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import helloWorld from './components/helloWorld.vue';
+export default {
+  component:{
+    helloWorld
+  }
+}
+</script>
+
+<template lang="">
+  <div>
+    <helloWorld />
+  </div>
+</template>
+
+<style lang="">
+  
+</style>
---
+++ src/components/helloWorld.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template lang="">
+    <div>
+        <h1>La mia prima app in Vite</h1>
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
